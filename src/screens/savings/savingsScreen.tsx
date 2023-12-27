568cb3752f9e3ff057f078866776dd51cc1882c2
MirRiyadh <riyadh.ju@gmail.com>
2023-12-27 21:54:26 +0600

    working on savings

--- src/screens/savings/savingsScreen.tsx
+++ src/screens/savings/savingsScreen.tsx
@@ -1,12 +1,20 @@
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import React, {useState} from 'react';
-import {Box, Image} from '@gluestack-ui/themed';
+import {Text, Box, Image, Button, ButtonText, MenuItem, Icon, MenuItemLabel} from '@gluestack-ui/themed';
 import GlueStackProvider from '../../gluestack_config/gluestackProvider';
 import CommonHeaderPlusBack from '../../custom/commonHeader/CommonHeaderPlusBack';
 const savingIcon = require('../../../assets/icons/budget.png');
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import { Menu } from '@gluestack-ui/themed';
+import { TouchableOpacity } from 'react-native';
+import CommonWriteBox from '../../custom/commonWriteBox/CommonWriteBox';
+import CustomModal from '../../custom/customModal/CustomModal';
+import CommonDateFilter from '../../custom/dateFilter/commonDateFilter';
 
-const SavingsScreen = () => {
+
+const SavingsScreen = ({navigation}: any) => {
   const [searchText, setSearchText] = useState('');
+  const [modal, setModal] = useState(false);
   const datas = [
     {
       id: '1',
@@ -28,18 +36,65 @@ const SavingsScreen = () => {
         setSearchText={setSearchText}
       />
       <Box height={'100%'} px={20} my={30}>
+      <CommonDateFilter />
         {datas.map((data, i) => {
           return (
-            <Box
+         <TouchableOpacity onPress={() => navigation.navigate('Savings-History')}>
+           <Box
               key={i}
               borderWidth={1}
               borderColor="$#DDDDDD"
               py={7}
               px={20}
               borderRadius={5}
-              mb={14}>
+              mb={14}
+              >
               <Box flexDirection="row" justifyContent="flex-end">
-                <Text>...</Text>
+                {/* _____________3 dots icon___________________ */}
+                
+                <Menu
+                  placement="bottom" 
+                  trigger={({ ...triggerProps }) => {
+                  return (
+                  <TouchableOpacity {...triggerProps}>
+                          <AntDesign
+                            name="ellipsis1"
+                            size={20}
+                            color="black"
+                          />
+                  </TouchableOpacity>
+
+            );
+          }}
+        >
+          <MenuItem key="Community" textValue="Community" >
+          <Text>
+                <AntDesign name="star" color="black" size={14} />
+                </Text>
+              <MenuItemLabel size='sm' ml={5}>
+                Edit
+              </MenuItemLabel>
+          </MenuItem>
+          <MenuItem key="Plugins" textValue="Plugins">
+              {/* PuzzleIcon is imported from 'lucide-react-native' */}
+              {/* <Icon as={PuzzleIcon} size="sm" mr='$2'/> */}
+              <MenuItemLabel size='sm'>
+                Delete
+              </MenuItemLabel>
+          </MenuItem>
+          <MenuItem key="Theme" textValue="Theme">
+              {/* PaintBucket is imported from 'lucide-react-native' */}
+              {/* <Icon as={PaintBucket} size="sm" mr='$2'/> */}
+              <MenuItemLabel size='sm'>
+                Add A Pin
+              </MenuItemLabel>
+          </MenuItem>
+          
+          
+        </Menu>
+
+
+                
               </Box>
               <Box
                 flexDirection="row"
@@ -63,7 +118,7 @@ const SavingsScreen = () => {
                     />
                   </Box>
                   <Box>
-                    <Text fontSize={14} borderColor={'$red'}>
+                    <Text>
                       Mobile-Bkash
                     </Text>
                     <Text>01734431369</Text>
@@ -77,13 +132,45 @@ const SavingsScreen = () => {
                 </Box>
                 {/* ------------arrow content box----------------- */}
                 <Box>
-                  <Text> → </Text>
+                  <Text> <AntDesign name="right" color="black" size={16} /> </Text>
                 </Box>
               </Box>
             </Box>
+          </TouchableOpacity>
+
+            
           );
         })}
+        <Box flexDirection='column' justifyContent='flex-end'>
+          <CommonWriteBox
+          icon={<AntDesign name="star" color="white" size={35} />}
+          title="Total Savings"
+          amount="1500"
+          btTitle="Add People"
+          modal={modal}
+          setModal={setModal}
+        />
+
+       <CustomModal
+          modalVisible={modal}
+          setModalVisible={setModal}
+          Radius={20}
+          height="80%"
+          width="90%"
+          appearance={true}
+          backButton={true}
+          // backButtonTitle="Modal Open hoise"
+         >
+          <Box>
+            <Text>Savings</Text>
+          </Box>
+        </CustomModal></Box>
+        
+
+
+        
       </Box>
+      
     </GlueStackProvider>
   );
 };
