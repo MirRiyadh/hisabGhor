4a8880e2234768511190b014bdc2b350d6f70b9d
MirRiyadh <riyadh.ju@gmail.com>
2023-12-31 20:02:30 +0600

    working on budget section

--- src/screens/budgets/budgetMainScreen.tsx
+++ src/screens/budgets/budgetMainScreen.tsx
@@ -2,20 +2,46 @@ import { View, Text } from 'react-native'
 import React, { useState } from 'react'
 import CommonWriteBox from '../../custom/commonWriteBox/CommonWriteBox'
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import GlueStackProvider from '../../gluestack_config/gluestackProvider';
+import { Box } from '@gluestack-ui/themed';
+import CommonHeaderPlusBack from '../../custom/commonHeader/CommonHeaderPlusBack';
 
 const BudgetMainScreen = () => {
-    const [modal, setModal] = useState(false);
+  const [searchText, setSearchText] = useState('');
+  const [modal, setModal] = useState(false);
+  const datas = [
+    {
+      id: '1',
+      img: '',
+      title: '',
+      number: '',
+      date: '',
+      amount: '',
+    },
+    {id: '2', img: '', title: '', number: '', date: '', amount: ''},
+  ];
   return (
-    <View>
-       <CommonWriteBox
+    <GlueStackProvider>
+      <Box>
+      <CommonHeaderPlusBack
+        isBack={true}
+        title="Budget"
+        isSearch={true}
+        searchText={searchText}
+        setSearchText={setSearchText}
+      />
+
+        <CommonWriteBox
           icon={<FontAwesome name="fax" color="white" size={30} />}
           title="Total"
           amount="10500"
           btTitle="Add Sector"
           modal={modal}
           setModal={setModal}
-        />
-    </View>
+         />
+      </Box>
+       
+    </GlueStackProvider>
   )
 }
 
