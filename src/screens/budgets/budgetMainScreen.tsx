91654e0377785f453f27b9966bcce70677defbb2
MirRiyadh <riyadh.ju@gmail.com>
2024-02-06 22:03:09 +0600

    update 3 modals

--- src/screens/budgets/budgetMainScreen.tsx
+++ src/screens/budgets/budgetMainScreen.tsx
@@ -1,6 +1,4 @@
-const cash = require('../../../assets/icons/cash-withdrawal.png');
-const history = require('../../../assets/icons/file.png');
-const money = require('../../../assets/icons/money-black.png');
+const addBudgetIcon = require('../../../assets/icons/add.png');
 const savingIcon = require('../../../assets/icons/budget.png');
 
 import { View } from 'react-native'
@@ -163,64 +161,74 @@ const BudgetMainScreen = ({navigation}: any) => {
           modalVisible={modal}
           setModalVisible={setModal}
           Radius={20}
-          height="65%"
+          height={400}
           width="90%"
           appearance={true}
-          // backButton={true}
+          backButton={true}
           // backButtonTitle="Modal Open hoise"
-        >
-          <Box>
-            <Box my="$2" justifyContent="center" alignItems="center">
-              <FontAwesome name="user" size={45} color="gray" />
+         >
+          <>
+          <Box my="$2" justifyContent="center" alignItems="center">
+              <Image  
+              w={60}
+              h={60}
+              m={'auto'}
+              source={addBudgetIcon}
+              alt="savings"/>
             </Box>
-
+          <ScrollView>
+            <Box>
             <VStack px="$1" gap="$4" mt="$3">
-              <Input rounded="$lg">
-                <InputField placeholder="Full Name" />
+              <Input rounded="$lg" >
+                <InputField placeholder="Sector Name" />
               </Input>
-              <Input rounded="$lg">
-                <InputField placeholder="Phone Number" />
-              </Input>
-              <Input rounded="$lg">
-                <InputField placeholder="Address" />
-              </Input>
+              
               <Input rounded="$lg">
                 <InputField placeholder="Amount" />
               </Input>
               <Input rounded="$lg">
                 <InputField placeholder="Reason" />
               </Input>
-              <Input rounded="$lg" w="$20">
-                <InputField placeholder="Date" />
+              
+              <Input rounded="$lg" w="25%" >
+                <InputField fontSize={16} placeholder="📅 Date" />
               </Input>
-              <HStack
-                gap="$3"
+              
+            </VStack>
+          </Box>
+          </ScrollView>
+          <HStack
+                gap="$5"
                 justifyContent="space-around"
                 alignItems="center"
-                mt="$3">
+                mt="$5"
+                pb="$2">
                 <Button
+                  backgroundColor='#4849BF'
                   action="positive"
-                  w="40%"
+                  w="45%"
                   onPress={() =>
                     RegisteredUser({
                       email: 'arifbiswas@gamil.com',
                       name: 'arifbiswas',
-                      password: 'lolmama',
+                      password: 'lmama',
                     })
                   }>
-                  <ButtonIcon as={AddIcon} size="xl" />
-                  <ButtonText px="$2">Add</ButtonText>
+                  <ButtonIcon as={AddIcon} size="xl"  />
+                  <ButtonText px="$2" fontSize={18} fontWeight='400'>ADD</ButtonText>
                 </Button>
                 <Button
-                  w="40%"
+                backgroundColor='#6f6fd9'
+                  w="45%"
                   action="negative"
                   onPress={() => setModal(false)}>
                   <ButtonIcon as={CloseIcon} size="xl" />
-                  <ButtonText px="$2">Cancel</ButtonText>
+                  <ButtonText px="$2" fontSize={18} fontWeight='400'>Cancel</ButtonText>
                 </Button>
-              </HStack>
-            </VStack>
-          </Box>
+          </HStack>
+          </>
+          
+          
         </CustomModal>
        </Box>
       </Box>
