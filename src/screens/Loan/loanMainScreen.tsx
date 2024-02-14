1dc1c41122fd337d24deaf3d6ee5954565d0bf53
MirRiyadh <riyadh.ju@gmail.com>
2024-02-14 23:12:30 +0600

    update data

--- src/screens/Loan/loanMainScreen.tsx
+++ src/screens/Loan/loanMainScreen.tsx
@@ -45,7 +45,8 @@ const LoanMainScreen = ({navigation}: any) => {
 
     },
     {id: '2', img: '', title: '', number: '', date: '', amount: ''},
-    {id: '3', img: '', title: '', number: '', date: '', amount: ''}
+    {id: '3', img: '', title: '', number: '', date: '', amount: ''},
+    {id: '4', img: '', title: '', number: '', date: '', amount: ''},
     
    
   ];
